7f64264ca1b8599bc40e36434d0a7c8cda2655b0
DavideGila <davide.gila@gmail.com>
2023-12-22 17:20:00 +0100

    created about blade and changed route

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+
+<h1>About Us</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam delectus excepturi iure animi nisi, impedit fuga, aliquam dolorem distinctio cumque nesciunt accusamus harum voluptatem libero tenetur, soluta consequatur illum vitae.</p>
+
+@endsection
